a332b57695da4959837f904f80cd854ba76dd4b4
lengyelb <lengyel.balazs.t@gmail.com>
2023-04-01 13:23:35 +0200

    Fixed GYAK05.py

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -1,28 +1,31 @@
+from typing import Optional, Tuple
+
 import numpy as np
 import seaborn as sns
+from matplotlib import pyplot
 from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
 
 
 class KNNClassifier:
-    def __init__(self, k: int, test_split_ratio: float) -> None:
+    def __init__(self, k: int, test_split_ratio: float):
         self.k = k
         self.test_split_ratio = test_split_ratio
 
-        self.x_train: np.ndarray = None
-        self.y_train: np.ndarray = None
+        self.x_train: Optional[np.ndarray] = None
+        self.y_train: Optional[np.ndarray] = None
 
-        self.x_test: np.ndarray = None
-        self.y_test: np.ndarray = None
+        self.x_test: Optional[np.ndarray] = None
+        self.y_test: Optional[np.ndarray] = None
 
-        self.y_preds: np.ndarray = None
+        self.y_preds: Optional[np.ndarray] = None
 
     @staticmethod
-    def load_csv(path: str, clean_dataset: bool = False) -> tuple[np.ndarray, np.ndarray]:
+    def load_csv(path: str, clean_dataset: bool = False) -> Tuple[np.ndarray, np.ndarray]:
         np.random.seed(42)
         dataset = np.genfromtxt(path, delimiter=',')
         np.random.shuffle(dataset)
-        x, y = dataset[:, :-1], dataset[:, -1]
+        x, y = dataset[:, :4], dataset[:, -1]
 
         if clean_dataset:
             x[np.isnan(x)] = 3.5
@@ -36,16 +39,17 @@ class KNNClassifier:
 
         return x, y
 
-    def train_test_split(self, features: np.ndarray, labels: np.ndarray) -> None:
+    def train_test_split(self, features: np.ndarray, labels: np.ndarray):
         test_size = int(len(features) * self.test_split_ratio)
         train_size = len(features) - test_size
         assert (len(features) == test_size + train_size)
+
         self.x_train, self.y_train = features[:train_size, :], labels[:train_size]
         self.x_test, self.y_test = features[train_size:train_size + test_size, :], \
-                                   labels[train_size:train_size + test_size]
+            labels[train_size:train_size + test_size]
 
     def euclidean(self, element_of_x: np.ndarray) -> np.ndarray:
-        return np.sqrt(np.sum((self.x_train - element_of_x) ** 2, axis=0))
+        return np.sqrt(np.sum((self.x_train - element_of_x) ** 2, axis=1))
 
     def predict(self, x_test: np.ndarray) -> None:
         labels_pred = []
@@ -71,9 +75,14 @@ class KNNClassifier:
         return self.k
 
 
-my_class = KNNClassifier(5, 0.2)
-a, b = KNNClassifier.load_csv("datasets/iris.csv", clean_dataset=True)
-print(a.shape, b.shape)
-my_class.train_test_split(a, b)
-my_class.predict(my_class.x_train)
-print(my_class.accuracy())
+# # region testing
+# csv_path = "datasets/iris.csv"
+# x_test, y_test = KNNClassifier.load_csv(csv_path, True)
+#
+# knn = KNNClassifier(3, 0.2)
+# knn.train_test_split(x_test, y_test)
+# knn.predict(knn.x_test)
+# print(knn.accuracy())
+# knn.confusion_matrix()
+# pyplot.show()
+# # endregion
