595ebbcadc7165fc6379be26f5f8f200200e3995
lengyelb <lengyel.balazs.t@gmail.com>
2023-04-10 12:18:40 +0200

    Implemented AI training into HAZI06.py

--- HAZI/HAZI06/HAZI06.py
+++ HAZI/HAZI06/HAZI06.py
@@ -17,39 +17,21 @@ X = data.iloc[:, :-1].values
 Y = data.iloc[:, -1].values.reshape(-1, 1)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.2, random_state=41)
 
-classifier = DecisionTreeClassifier(min_samples_split=3, max_depth=3)
-classifier.fit(X_train, Y_train)
+results = []
 
-Y_pred = classifier.predict(X_test)
-print(accuracy_score(Y_test, Y_pred))
+for min_sample_split in range(1, 6):
+    for max_depth in range(1, 5):
+        classifier = DecisionTreeClassifier(min_samples_split=min_sample_split, max_depth=max_depth)
+        classifier.fit(X_train, Y_train)
 
+        Y_pred = classifier.predict(X_test)
+        intermediate_result = (min_sample_split, max_depth, accuracy_score(Y_test, Y_pred))
+        print(intermediate_result)
+        results.append(intermediate_result)
 
-"""
-3.  A feladatot a HAZI06.py-ban old meg.
-    Az órán megírt DecisionTreeClassifier-t fit-eld fel az első feladatban lementett csv-re.
-    A feladat célja az, hogy határozzuk meg azt, hogy a vonatok késnek-e vagy sem. 0p <= x < 5p --> nem késik (0), ha 5p <= x --> késik (1).
-    Az adatoknak a 20% legyen test és a splitelés random_state-je pedig 41 (mint órán)
-    A testset-en 80% kell elérni. Ha megvan a minimum százalék, akkor azzal paraméterezd fel a decisiontree-t és azt kell leadni.
-
-    A leadásnál csak egy fit kell, ezt azzal a paraméterre paraméterezd fel, amivel a legjobb accuracy-t elérted.
-
-    A helyes paraméter megtalálásához használhatsz grid_search-öt.
-    https://www.w3schools.com/python/python_ml_grid_search.asp
-
-4.  A tanításodat foglald össze 4-5 mondatban a HAZI06.py-ban a fájl legalján kommentben. Írd le a nehézségeket, mivel próbálkoztál, mi vált be és mi nem. Ezen kívül írd le 10 fitelésed eredményét is, hogy milyen paraméterekkel probáltad és milyen accuracy-t értél el.
-Ha ezt feladatot hiányzik, akkor nem fogadjuk el a házit!
-
-HAZI-
-    HAZI06-
-        -NJCleaner.py
-        -HAZI06.py
-
-##################################################################
-##                                                              ##
-## A feladatok közül csak a NJCleaner javítom unit test-el      ##
-## A decision tree-t majd manuálisan fogom lefuttatni           ##
-## NJCleaner - 10p, Tanítás - acc-nál 10%-ként egy pont         ##
-## Ha a 4. feladat hiányzik, akkor nem tudjuk elfogadni a házit ##
-##                                                              ##
-##################################################################
-"""
+print(results)
+print(max(results, key=lambda x: x[2]))
+with open("datasets/results.txt", 'w') as results_file:
+    sorted_results = sorted(results, key=lambda x: x[2], reverse=True)
+    for result_line in sorted_results:
+        results_file.write(', '.join(map(str, result_line)) + "\n")
