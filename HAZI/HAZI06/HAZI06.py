cbe7619d093b0531ee7dd90c4295b9b49b1a9ea2
lengyelb <lengyel.balazs.t@gmail.com>
2023-04-14 17:23:34 +0200

    Fixed bugs

--- HAZI/HAZI06/HAZI06.py
+++ HAZI/HAZI06/HAZI06.py
@@ -18,21 +18,27 @@ X = data.iloc[:, :-1].values
 Y = data.iloc[:, -1].values.reshape(-1, 1)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.2, random_state=41)
 
-results = []
+classifier = DecisionTreeClassifier(min_samples_split=2, max_depth=4)
+classifier.fit(X_train, Y_train)
 
-for min_sample_split in range(1, 6):
-    for max_depth in range(1, 5):
-        classifier = DecisionTreeClassifier(min_samples_split=min_sample_split, max_depth=max_depth)
-        classifier.fit(X_train, Y_train)
+Y_pred = classifier.predict(X_test)
+print(accuracy_score(Y_test, Y_pred))
 
-        Y_pred = classifier.predict(X_test)
-        intermediate_result = (min_sample_split, max_depth, accuracy_score(Y_test, Y_pred))
-        print(intermediate_result)
-        results.append(intermediate_result)
-
-print(results)
-print(max(results, key=lambda x: x[2]))
-with open("datasets/results.txt", 'w') as results_file:
-    sorted_results = sorted(results, key=lambda x: x[2], reverse=True)
-    for result_line in sorted_results:
-        results_file.write(', '.join(map(str, result_line)) + "\n")
+# results = []
+#
+# for min_sample_split in range(1, 6):
+#     for max_depth in range(1, 5):
+#         classifier = DecisionTreeClassifier(min_samples_split=min_sample_split, max_depth=max_depth)
+#         classifier.fit(X_train, Y_train)
+#
+#         Y_pred = classifier.predict(X_test)
+#         intermediate_result = (min_sample_split, max_depth, accuracy_score(Y_test, Y_pred))
+#         print(intermediate_result)
+#         results.append(intermediate_result)
+#
+# print(results)
+# print(max(results, key=lambda x: x[2]))
+# with open("datasets/results.txt", 'w') as results_file:
+#     sorted_results = sorted(results, key=lambda x: x[2], reverse=True)
+#     for result_line in sorted_results:
+#         results_file.write(', '.join(map(str, result_line)) + "\n")
